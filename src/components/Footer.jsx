04cd638b3cf7ec2f7c99c94b3039f366cca545b1
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-28 16:58:52 -0700

    finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
  
  const Footer = () => {
    return (
-     <div className="w-full flex items-center justify-center mx-10">
+     <div className="w-full flex items-center justify-center p-8">
         Develop with care by Trique Nguyen &copy; {new Date().getFullYear()} 
      </div>
    )
